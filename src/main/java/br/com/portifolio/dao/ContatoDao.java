751cf4c762f6c4bebdfd6c2c11c96f5a7bcce28c
PeterAbenza <abenzpeter@gmail.com>
2025-01-11 14:24:44 -0300

    add experiences

--- src/main/java/br/com/portifolio/dao/ContatoDao.java
+++
@@ -1,9 +0,0 @@
-package br.com.portifolio.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.portifolio.model.Contatos;
-
-public interface ContatoDao extends JpaRepository<Contatos, Integer> {
-
-}
